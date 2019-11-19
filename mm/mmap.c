24aad87705f821927bd45273e318493f774ae5e9
Anastasios Papagiannis <apapag@ics.forth.gr>
2019-11-19 21:40:09 +0200

    [v5,08/22] mm: RCU free VMAs (fixes)

--- mm/mmap.c
+++ mm/mmap.c
@@ -456,7 +456,7 @@ static void __vma_rb_erase(struct vm_area_struct *vma, struct mm_struct *mm)
 }
 
 static __always_inline void vma_rb_erase_ignore(struct vm_area_struct *vma,
-						struct rb_root *root,
+						struct mm_struct *mm,
 						struct vm_area_struct *ignore)
 {
 	/*
@@ -470,15 +470,15 @@ static __always_inline void vma_rb_erase_ignore(struct vm_area_struct *vma,
 }
 
 static __always_inline void vma_rb_erase(struct vm_area_struct *vma,
-					 struct rb_root *root)
+					 struct mm_struct *mm)
 {
 	/*
 	 * All rb_subtree_gap values must be consistent prior to erase,
 	 * with the possible exception of the vma being erased.
 	 */
-	validate_mm_rb(root, vma);
+	validate_mm_rb(&mm->mm_rb, vma);
 
-	__vma_rb_erase(vma, root);
+	__vma_rb_erase(vma, mm);
 }
 
 /*
